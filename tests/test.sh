78a9f64482438c58b4aa89cb00a95d69ce040e41
Manuel Thalmann <m@nuth.ch>
2022-03-23 00:46:04 +0100

    Set tests back to normal

--- tests/test.sh
+++ tests/test.sh
@@ -3,4 +3,4 @@ outputPath=/tmp/test-output
 apache2-foreground &
 su www-data -s /bin/bash -c "composer create-project -n silverstripe/installer:4.10.0 ."
 wget -O $outputPath --content-on-error localhost
-! cat $outputPath | grep -i "SS_DATABASE_CHOOSE_NAME"
+! cat $outputPath | grep -i "deprecated"
