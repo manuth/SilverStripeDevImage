3c034b15405b11f7476820a7bb778f1df5f0d718
Manuel Thalmann <m@nuth.ch>
2022-03-23 00:21:33 +0100

    Print the resulting output to the console

--- tests/test.sh
+++ tests/test.sh
@@ -2,4 +2,5 @@
 apache2-foreground &
 su www-data -s /bin/bash -c "composer create-project -n silverstripe/installer:4.10.0 ."
 wget -O /tmp/test-output --content-on-error localhost
+cat /tmp/test-output
 ! cat /tmp/test-output | grep -i "deprecated"
