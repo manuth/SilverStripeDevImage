3d63582759c74c8389ed0f1164fc2ff541807d94
Manuel Thalmann <m@nuth.ch>
2022-03-22 22:48:36 +0100

    Fix broken test

--- tests/test.sh
+++ tests/test.sh
@@ -1,4 +1,4 @@
 apache2-foreground &
 su www-data -s /bin/bash -c "composer create-project -n silverstripe/installer:4.10.0 ."
-wget -O /tmp/test-output localhost
+wget -O /tmp/test-output --content-on-error localhost
 ! cat /tmp/test-output | grep -i "deprecated"
