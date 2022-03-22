9643b7ef8ccfdf772f5db58e0085a3bf74c48b6e
Manuel Thalmann <m@nuth.ch>
2022-03-23 00:21:45 +0100

    Remove redundancies from the test script

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+outputPath=/tmp/test-output
 apache2-foreground &
 su www-data -s /bin/bash -c "composer create-project -n silverstripe/installer:4.10.0 ."
-wget -O /tmp/test-output --content-on-error localhost
-cat /tmp/test-output
-! cat /tmp/test-output | grep -i "deprecated"
+wget -O $outputPath --content-on-error localhost
+cat $outputPath
+! cat $outputPath | grep -i "deprecated"
